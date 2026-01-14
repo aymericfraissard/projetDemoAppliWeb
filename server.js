10695cf344002da8ff536dea47743058b5097c6f
aymericfraissard <aymericfraissard@gmail.com>
2026-01-14 11:29:34 +0100

    commit register

--- server.js
+++ server.js
@@ -2,7 +2,11 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello, World!');
+    res.send('bienvenue sur la page de login');
+});
+
+app.post('/register', (req, res) => {
+    res.send('merci de vous créer un compte !');
 });
 
 app.listen(3000, () => {
